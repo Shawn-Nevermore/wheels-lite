82ea76e131ad18bdffd55aef3fecf0d43bb7b9b0
Shawn-Nevermore <shawnxu2016sg@gmail.com>
2020-06-27 17:46:18 +0800

    添加icon，iconPostion属性

--- src/button.vue
+++ src/button.vue
@@ -1,25 +1,56 @@
 <template>
-  <button class="g-button">按钮</button>
+    <button class="g-button" :class="{ [`icon-${iconPosition}`]: true }">
+        <svg v-if="icon" class="icon" aria-hidden="true">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
-export default {};
+export default {
+    props: ['icon', 'iconPosition'],
+}
 </script>
 <style lang="scss">
 .g-button {
-  height: var(--button-height);
-  font-size: var(--font-size);
-  padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
-  &:hover {
-    border-color: var(--border-hover-color);
-  }
-  &:active {
-    background-color: var(--button-active-bg);
-  }
-  &:focus {
-    outline: none;
-  }
+    height: var(--button-height);
+    font-size: var(--font-size);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    display: inline-flex;
+    vertical-align: middle; // 内联元素，不能使用默认值，否则对不齐
+    justify-content: center;
+    align-items: center;
+    &:hover {
+        border-color: var(--border-hover-color);
+    }
+    &:active {
+        background-color: var(--button-active-bg);
+    }
+    &:focus {
+        outline: none;
+    }
+    > .icon {
+        order: 1;
+        margin-right: 0.2em;
+    }
+    > .content {
+        order: 2;
+    }
+    &.icon-right {
+        > .icon {
+            order: 2;
+            margin-right: 0;
+            margin-left: 0.2em;
+        }
+
+        > .content {
+            order: 1;
+        }
+    }
 }
 </style>
